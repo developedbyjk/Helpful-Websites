b6f645f4c52cc73e1af091082148825f277181dd
developedbyjk <71823598+developedbyjk@users.noreply.github.com>
2024-02-19 22:54:54 +0530

    commit

--- src/data.js
+++ src/data.js
@@ -20,5 +20,13 @@ export const data = [
        desc: ' Ask Documents',
          link : 'https://www.docu-ask.com/'
     },
+    {
+        id: 4,
+       icon: 'http://www.google.com/s2/favicons?domain=www.animstats.com',
+       name: 'Anim Stats',
+       desc: 'Make Animations',
+         link : 'https://www.animstats.com/'
+    },
+    
 
 ]
